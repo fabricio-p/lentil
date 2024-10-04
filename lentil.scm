86e7c9e25a3942f2cbf97306edae459b99293396
fabricio-p <fabriciopashaj.dev@gmail.com>
2024-10-04 21:34:37 +0300

    Static index-based variable lookup

--- lentil.scm
+++ lentil.scm
@@ -8,41 +8,43 @@
 
 (export compile)
 
-(define (compile raw next)
+(define (compile raw env next)
   (match raw
-    [(? symbol?) `(refer ,raw ,next)]
+    [(? symbol?) `(refer ,(compile-lookup raw env) ,next)]
 
     [('quote val) `(constant ,val ,next)]
 
     [('lambda vars body)
-     (list 'close vars (compile body '(return)) next)]
+     (list 'close vars (compile body (extend env vars) '(return)) next)]
 
     [('if test then else)
-     (let ([thenc (compile then next)]
-	   [elsec (compile else next)])
-       (compile test `(test ,thenc ,elsec)))]
+     (let ([thenc (compile then env next)]
+	   [elsec (compile else env next)])
+       (compile test env `(test ,thenc ,elsec)))]
 
     [('set! var raw-val)
-     (compile raw-val `(assign ,var ,next))]
+     (let ([access (compile-lookup raw env)])
+       (compile raw-val env `(assign ,access ,next)))]
 
     [('call/cc raw-cb)
-     (let ([compiled `(conti (argument ,(compile raw-cb '(apply))))])
+     (let ([compiled `(conti (argument ,(compile raw-cb env '(apply))))])
        (if (tail-instr? next)
 	   compiled
 	   `(frame ,next ,compiled)))]
 
     [('let vars body)
-     (compile (cons (list 'lambda (map car vars) body) (map cadr vars)) next)]
+     (compile (cons (list 'lambda (map car vars) body) (map cadr vars)) env next)]
 
     [(? pair?)
      (let loop ([args (cdr raw)]
-		[compiled (compile (car raw) '(apply))])
+		[compiled (compile (car raw) env '(apply))])
        (if (null? args)
 	   (if (tail-instr? next)
 	       compiled
 	       `(frame ,next ,compiled))
 	   (loop (cdr args)
 		 (compile (car args)
+			  env
 			  `(argument ,compiled)))))]
 
     [_ `(constant ,raw ,next)]))
@@ -51,20 +53,20 @@
   (match next-expr
     [('halt) acc]
     
-    [('refer var next-expr)
-     (interpret-asm (car (lookup var env)) next-expr env val-rib stack)]
+    [('refer access next-expr)
+     (interpret-asm (car (lookup access env)) next-expr env val-rib stack)]
     
     [('constant val next-expr)
      (interpret-asm val next-expr env val-rib stack)]
     
     [('close vars body next-expr)
-     (interpret-asm (make-closure body env vars) next-expr env val-rib stack)]
+     (interpret-asm (make-closure body env) next-expr env val-rib stack)]
 
     [('test then else)
      (interpret-asm acc (if acc then else) env val-rib stack)]
 
-    [('assign var next-expr)
-     (set-car! (lookup var env) acc)
+    [('assign access next-expr)
+     (set-car! (lookup access env) acc)
      (interpret-asm acc next-expr env val-rib stack)]
 
     [('conti next-expr)
@@ -81,8 +83,8 @@
 
     [('apply)
      (match acc
-       [#('closure body env vars) acc
-	(interpret-asm acc body (extend env vars val-rib) '() stack)]
+       [#('closure body env) acc
+	(interpret-asm acc body (extend env val-rib) '() stack)]
        [(? procedure?)
 	(acc val-rib)])]
 
@@ -90,30 +92,38 @@
      (match-let ([(next-expr env val-rib stack) stack])
        (interpret-asm acc next-expr env val-rib stack))]))
 
-(define (make-closure body env vars)
-  (vector 'closure body env vars))
+(define (make-closure body env)
+  (vector 'closure body env))
 
 (define (make-continuation stack)
-  (make-closure `(nuate ,stack v) '() '(v)))
+  (make-closure `(nuate ,stack (0 . 0)) '()))
 
-(define (lookup var env)
-  (let next-rib ([env env])
-    (let next-elem ([vars (caar env)] [vals (cdar env)])
+(define (lookup access env)
+  (let next-rib ([env env] [rib (car access)])
+    (if (= rib 0)
+	(let next-elem ([rib (car env)] [elem (cdr access)])
+	  (if (= 0 elem)
+	      rib
+	      (next-elem (cdr rib) (- elem 1))))
+	(next-rib (cdr env) (- rib 1)))))
+
+(define (compile-lookup var env)
+  (let next-rib ([env env] [rib 0])
+    (let next-elem ([vars (car env)] [elem 0])
       (cond
-       [(null? vars) (next-rib (cdr env))]
-       [(eq? (car vars) var) vals]
-       [else (next-elem (cdr vars) (cdr vals))]))))
+       [(null? vars) (next-rib (cdr env) (+ rib 1))]
+       [(eq? (car vars) var) (cons rib elem)]
+       [else (next-elem (cdr vars) (+ elem 1))]))))
 
 (define (call-frame next-expr env val-rib stack)
   (list next-expr env val-rib stack))
 
-(define (extend env vars vals)
-  (cons (cons vars vals) env))
+(define (extend env rib)
+  (cons rib env))
 
 (define (tail-instr? next) (eq? (car next) 'return))
 
-#|(interpret-asm #f (compile '(+ 1 2) '(halt))
-	       (list (cons '(+)
-			   (list (lambda (rib) (+ (car rib) (cadr rib))))))
+#|(interpret-asm #f (compile '(let ([a 10] [b 20]) (+ a b)) '((+)) '(halt))
+	       (list (list (lambda (rib) (+ (car rib) (cadr rib)))))
 	       '()
 	       '())|#
